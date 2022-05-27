05d107b9886adb97fe860dfbfeb89da11937428f
gdlcf88 <gdlcf88@outlook.com>
2022-05-28 04:18:12 +0800

    Fix DtmMongoDbGrpcService

--- src/EasyAbp.Abp.EventBus.Boxes.Dtm.MongoDB/EasyAbp/Abp/EventBus/Boxes/Dtm/Services/DtmMongoDbGrpcService.cs
+++ src/EasyAbp.Abp.EventBus.Boxes.Dtm.MongoDB/EasyAbp/Abp/EventBus/Boxes/Dtm/Services/DtmMongoDbGrpcService.cs
@@ -1,8 +1,8 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using DtmCommon;
-using Dtmgrpc;
-using DtmMongoBarrier;
+using EasyAbp.Abp.EventBus.Boxes.Dtm.Barriers;
 using EasyAbp.Abp.EventBus.Boxes.Dtm.MongoDB;
 using Grpc.Core;
 using Microsoft.Extensions.DependencyInjection;
@@ -21,29 +21,29 @@ public class DtmMongoDbGrpcService : MongoDB.DtmMongoDbGrpcService.DtmMongoDbGrp
     
     protected IServiceProvider ServiceProvider { get; }
 
-    protected IBranchBarrierFactory BranchBarrierFactory { get; }
-    
     protected IActionApiTokenChecker ActionApiTokenChecker { get; }
 
     protected IConnectionStringHasher ConnectionStringHasher { get; }
     
+    protected IAbpMongoDbDtmMsgBarrierManager AbpMongoDbDtmMsgBarrierManager { get; }
+
     protected IConnectionStringResolver ConnectionStringResolver { get; }
 
     public DtmMongoDbGrpcService(
         ILogger<DtmMongoDbGrpcService> logger,
         ICurrentTenant currentTenant,
         IServiceProvider serviceProvider,
-        IBranchBarrierFactory branchBarrierFactory,
         IActionApiTokenChecker actionApiTokenChecker,
         IConnectionStringHasher connectionStringHasher,
+        IAbpMongoDbDtmMsgBarrierManager abpMongoDbDtmMsgBarrierManager,
         IConnectionStringResolver connectionStringResolver)
     {
         Logger = logger;
         CurrentTenant = currentTenant;
         ServiceProvider = serviceProvider;
-        BranchBarrierFactory = branchBarrierFactory;
         ActionApiTokenChecker = actionApiTokenChecker;
         ConnectionStringHasher = connectionStringHasher;
+        AbpMongoDbDtmMsgBarrierManager = abpMongoDbDtmMsgBarrierManager;
         ConnectionStringResolver = connectionStringResolver;
     }
 
@@ -54,8 +54,6 @@ public class DtmMongoDbGrpcService : MongoDB.DtmMongoDbGrpcService.DtmMongoDbGrp
             return new MongoDB.DtmStyleResponse { DtmResult = Constant.ResultFailure };
         }
         
-        var barrier = BranchBarrierFactory.CreateBranchBarrier(context, Logger);
-
         var tenantId = request.Info.TenantId.IsNullOrEmpty() ? (Guid?)null : Guid.Parse(request.Info.TenantId);
 
         using var changeTenant = CurrentTenant.Change(tenantId);
@@ -73,10 +71,13 @@ public class DtmMongoDbGrpcService : MongoDB.DtmMongoDbGrpcService.DtmMongoDbGrp
         {
             return new MongoDB.DtmStyleResponse { DtmResult = Constant.ResultFailure };
         }
+        
+        var gid = context.RequestHeaders.FirstOrDefault(m => m.Key.Equals("dtm-gid"))?.Value ??
+                  throw new DtmException("Cannot get dtm-gid from the gRPC request headers.");
 
         return new MongoDB.DtmStyleResponse
         {
-            DtmResult = await barrier.MongoQueryPrepared(dbContext.Client)
+            DtmResult = await AbpMongoDbDtmMsgBarrierManager.QueryPreparedAsync(dbContext, gid)
         };
     }
 }
