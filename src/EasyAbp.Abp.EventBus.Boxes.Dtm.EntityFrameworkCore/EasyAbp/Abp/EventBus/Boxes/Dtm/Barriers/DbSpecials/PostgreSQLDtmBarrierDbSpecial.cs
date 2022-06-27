9fd006a9f61ecd4f2f67efd69d79fd1d21e60d46
Super <47396430@qq.com>
2022-06-27 18:04:44 +0800

    Fix PostgreSQLDtmBarrierDbSpecial

--- src/EasyAbp.Abp.EventBus.Boxes.Dtm.EntityFrameworkCore/EasyAbp/Abp/EventBus/Boxes/Dtm/Barriers/DbSpecials/PostgreSQLDtmBarrierDbSpecial.cs
+++ src/EasyAbp.Abp.EventBus.Boxes.Dtm.EntityFrameworkCore/EasyAbp/Abp/EventBus/Boxes/Dtm/Barriers/DbSpecials/PostgreSQLDtmBarrierDbSpecial.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using DtmCommon;
 using EasyAbp.Abp.EventBus.Boxes.Dtm.Options;
 
@@ -10,20 +10,20 @@ public class PostgreSQLDtmBarrierDbSpecial : PostgresDBSpecial, IDtmBarrierDbSpe
     public static string DtmSequenceName { get; set; } = "barrier_seq";
     
     public static string DtmBarrierTableAndValueSqlFormat { get; set; } =
-        "\"{0}\"(trans_type, gid, branch_id, op, barrier_id, reason) values(@trans_type,@gid,@branch_id,@op,@barrier_id,@reason)";
+        "{0}(trans_type, gid, branch_id, op, barrier_id, reason) values(@trans_type,@gid,@branch_id,@op,@barrier_id,@reason)";
     
     public static string QueryPreparedSqlFormat { get; set; } =
-        "select reason from \"{0}\" where gid=@gid and branch_id=@branch_id and op=@op and barrier_id=@barrier_id";
+        "select reason from {0} where gid=@gid and branch_id=@branch_id and op=@op and barrier_id=@barrier_id";
     
     public virtual string GetCreateBarrierTableSql(AbpDtmEventBoxesOptions options)
     {
         var configuredTableName = options.BarrierTableName ?? DefaultBarrierTableName;
         var split = configuredTableName.Split('.', 2, StringSplitOptions.RemoveEmptyEntries);
 
-        var schemaName = split.Length == 2 ? split[0] : null;
-        var tableName = split.Length == 2 ? split[1] : configuredTableName;
+        var schemaName = split.Length == 2 ? $"\"{split[0]}\"" : null;
+        var tableName = split.Length == 2 ? $"\"{split[1]}\"" : $"\"{configuredTableName}\"";
         var tableFullName = schemaName is null ? tableName : $"{schemaName}.{tableName}";
-        var sequenceName = DtmSequenceName;
+        var sequenceName = $"\"{DtmSequenceName}\"";
         var sequenceFullName = schemaName is null ? sequenceName : $"{schemaName}.{sequenceName}";
 
         var sql = string.Empty;
@@ -31,7 +31,7 @@ public class PostgreSQLDtmBarrierDbSpecial : PostgresDBSpecial, IDtmBarrierDbSpe
         if (schemaName is not null)
         {
             sql += $@"
-create schema if not exists ""{schemaName}"";
+create schema if not exists {schemaName};
 ";
         }
         
